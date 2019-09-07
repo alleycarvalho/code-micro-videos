6a7e0ad16615c3aac9162e71c1d6fb88dfa53c9e
Alley M. Carvalho <alleycarvalho@gmail.com>
2019-09-07 11:40:19 -0300

    Integration tests for Category Controller

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -15,7 +15,10 @@ class CategoryController extends Controller
 
     public function store(CategoryRequest $request)
     {
-        return Category::create($request->all());
+        $category = Category::create($request->all());
+        $category->refresh();
+
+        return $category;
     }
 
     public function show(Category $category)
